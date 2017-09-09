45b4acc785efaeb62b6a487e2b69d42ea4d72e48
before30 <before30@gmail.com>
2017-09-09 20:27:48 +0900

    Add consumer funcs

--- src/main/java/cc/before30/fpij/utils/Funs.java
+++ src/main/java/cc/before30/fpij/utils/Funs.java
@@ -4,6 +4,7 @@ import cc.before30.fpij.types.*;
 
 import java.util.Comparator;
 import java.util.Objects;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -256,5 +257,78 @@ public class Funs {
         return o -> f.apply(o, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10);
     }
 
+    public static <O, T1> Consumer<O> accepting(final Consumer2<? super O, ? super T1> c, final T1 t1) {
+        Objects.requireNonNull(c, "The Consumer cannot be null.");
+        return o -> c.accept(o, t1);
+    }
 
+    public static <O, T1, T2> Consumer<O> accepting(final Consumer3<? super O, ? super T1, ? super T2> c, final T1 t1, final T2 t2) {
+        Objects.requireNonNull(c, "The Consumer cannot be null.");
+        return o -> c.accept(o, t1, t2);
+    }
+
+    public static <O, T1, T2, T3> Consumer<O> accepting(final Consumer4<? super O, ? super T1, ? super T2, ? super T3> c,
+                                                        final T1 t1, final T2 t2, final T3 t3) {
+        Objects.requireNonNull(c, "The Consumer cannot be null.");
+        return o -> c.accept(o, t1, t2, t3);
+    }
+
+    public static <O, T1, T2, T3, T4> Consumer<O> accepting(final Consumer5<? super O,
+            ? super T1, ? super T2, ? super T3, ? super T4> c,
+                                                        final T1 t1, final T2 t2, final T3 t3, final T4 t4) {
+        Objects.requireNonNull(c, "The Consumer cannot be null.");
+        return o -> c.accept(o, t1, t2, t3, t4);
+    }
+
+    public static <O, T1, T2, T3, T4, T5> Consumer<O> accepting(final Consumer6<? super O,
+            ? super T1, ? super T2, ? super T3, ? super T4, ? super T5> c,
+                                                            final T1 t1, final T2 t2, final T3 t3, final T4 t4, final T5 t5) {
+        Objects.requireNonNull(c, "The Consumer cannot be null.");
+        return o -> c.accept(o, t1, t2, t3, t4, t5);
+    }
+
+    public static <O, T1, T2, T3, T4, T5, T6> Consumer<O> accepting(final Consumer7<? super O,
+            ? super T1, ? super T2, ? super T3, ? super T4, ? super T5,
+            ? super T6> c,
+                                                                final T1 t1, final T2 t2, final T3 t3, final T4 t4, final T5 t5,
+                                                                final T6 t6) {
+        Objects.requireNonNull(c, "The Consumer cannot be null.");
+        return o -> c.accept(o, t1, t2, t3, t4, t5, t6);
+    }
+
+    public static <O, T1, T2, T3, T4, T5, T6, T7> Consumer<O> accepting(final Consumer8<? super O,
+            ? super T1, ? super T2, ? super T3, ? super T4, ? super T5,
+            ? super T6, ? super T7> c,
+                                                                    final T1 t1, final T2 t2, final T3 t3, final T4 t4, final T5 t5,
+                                                                    final T6 t6, final T7 t7) {
+        Objects.requireNonNull(c, "The Consumer cannot be null.");
+        return o -> c.accept(o, t1, t2, t3, t4, t5, t6, t7);
+    }
+
+    public static <O, T1, T2, T3, T4, T5, T6, T7, T8> Consumer<O> accepting(final Consumer9<? super O,
+            ? super T1, ? super T2, ? super T3, ? super T4, ? super T5,
+            ? super T6, ? super T7, ? super T8> c,
+                                                                        final T1 t1, final T2 t2, final T3 t3, final T4 t4, final T5 t5,
+                                                                        final T6 t6, final T7 t7, final T8 t8) {
+        Objects.requireNonNull(c, "The Consumer cannot be null.");
+        return o -> c.accept(o, t1, t2, t3, t4, t5, t6, t7, t8);
+    }
+
+    public static <O, T1, T2, T3, T4, T5, T6, T7, T8, T9> Consumer<O> accepting(final Consumer10<? super O,
+            ? super T1, ? super T2, ? super T3, ? super T4, ? super T5,
+            ? super T6, ? super T7, ? super T8, ? super T9> c,
+                                                                            final T1 t1, final T2 t2, final T3 t3, final T4 t4, final T5 t5,
+                                                                            final T6 t6, final T7 t7, final T8 t8, final T9 t9) {
+        Objects.requireNonNull(c, "The Consumer cannot be null.");
+        return o -> c.accept(o, t1, t2, t3, t4, t5, t6, t7, t8, t9);
+    }
+
+    public static <O, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> Consumer<O> accepting(final Consumer11<? super O,
+            ? super T1, ? super T2, ? super T3, ? super T4, ? super T5,
+            ? super T6, ? super T7, ? super T8, ? super T9, ? super T10> c,
+                                                                                final T1 t1, final T2 t2, final T3 t3, final T4 t4, final T5 t5,
+                                                                                final T6 t6, final T7 t7, final T8 t8, final T9 t9, final T10 t10) {
+        Objects.requireNonNull(c, "The Consumer cannot be null.");
+        return o -> c.accept(o, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10);
+    }
 }
